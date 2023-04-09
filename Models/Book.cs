6cdcaa4323e7dfb147163f17c6db29957b4385fc
assemAA <assemamr49@yahoo.com>
2023-04-09 14:32:23 +0200

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksEccommerce.Models
+{
+    public class Book
+    {
+        [Key]
+        public  int id { get; set; }
+        public string name { get; set; }    
+        public string description { get; set; }
+        public string category { get; set; }
+        public double price { get; set; } 
+
+        public IList<Book_User> bookUser { get; set;} 
+        
+    }
+}
---
+++ Models/BooksDB.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksEccommerce.Models
+{
+    public class BooksDB : IdentityDbContext<User>
+    {
+       public  BooksDB() : base()
+       { 
+        
+       }
+        
+       public BooksDB(DbContextOptions options) : base(options) { }
+
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+       {
+            base.OnModelCreating(modelBuilder);
+       }
+
+       public DbSet<Book> Books { get; set;}
+       public DbSet<User> clients { get; set;}
+
+       public DbSet<Book_User> booksUsers { get;set;}
+
+    }
+}
